1aab363425af64ded7185ff639655a805389a62d
Patrick Steadman <ptsteadman@gmail.com>
2022-02-09 20:29:35 -0500

    Create simplified example without span
    Signed-off-by: Patrick Steadman <ptsteadman@gmail.com>

--- dir/requestWithSpan.d.ts
+++ dir/requestWithSpan.d.ts
@@ -1,11 +1,10 @@
+export {};
 
 // Extend the Request type in Express to include the Span
-import { Span } from 'opentracing';
-
 declare global {
     namespace Express {
         export interface Request {
-            span?: Span;
+            foo?: string;
         }
     }
 }
--- dir/testServer.ts
+++ dir/testServer.ts
@@ -1,29 +1,9 @@
 import express from 'express';
-// import { ParamsDictionary } from 'express-serve-static-core';
-import * as Sentry from '@sentry/node';
-import { Tracer } from 'opentracing';
 
-export default function opentracingSentryMiddleware(
-    _tracer: Tracer,
-): (
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction,
-) => void {
-    return (
-        req: express.Request,
-        _res: express.Response,
-        next: express.NextFunction,
-    ) => {
-        const span = req.span;
+const app = express();
 
-        if (span !== undefined) {
-            Sentry.configureScope(function (scope) {
-                scope.setExtra('trace_id', span.context().toTraceId());
-                next();
-            });
-        } else {
-            next();
-        }
-    };
-}
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('Hello ' + req.foo);
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
