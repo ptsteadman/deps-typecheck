fa193905b17bb84297def78f757a42ecbd739ce1
Patrick Steadman <ptsteadman@gmail.com>
2022-02-18 11:10:11 -0500

    Do not import 'a' so server will run
    Signed-off-by: Patrick Steadman <ptsteadman@gmail.com>

--- dir/testServer.ts
+++ dir/testServer.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-import { a } from './a'
 
 const app = express();
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    console.log(a)
     res.send('Hello ' + req.foo);
 });
 
